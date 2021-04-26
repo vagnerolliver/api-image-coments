94a8aa9fd19a1c9f41dd41ef736eed6051e401fe
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-26 16:46:47 -0300

    config: @ alias to jest and ts

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,9 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
