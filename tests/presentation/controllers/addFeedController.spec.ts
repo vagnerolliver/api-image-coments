19bc33711bd2baa0b025a01b78a9ad57831b9512
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-26 16:52:09 -0300

    test: ensure AddFeedController return an error if no url is provided

--- tests/presentation/controllers/addFeedController.spec.ts
+++ tests/presentation/controllers/addFeedController.spec.ts
@@ -11,5 +11,6 @@ describe('AddFeedController', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: url'))
   })
 })
