a26bbc638d663089ccf1688448386dd9d02e1314
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-27 21:15:37 -0300

    refactor: make AddFeedController conforms to a protocol

--- src/presentation/controllers/feed/addFeedController.ts
+++ src/presentation/controllers/feed/addFeedController.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from '@/presentation/errors/missingParamError'
 import { badRequest } from '@/presentation/helpers/httpHelper'
+import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 
-export class AddFeedController {
+export class AddFeedController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['url', 'description']
     for (const field of requiredFields) {
