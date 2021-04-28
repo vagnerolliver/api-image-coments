e8d0da66c93932d2d3649463ee081df69fe51c27
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-28 09:13:05 -0300

    test: ensure AddFeedController return 400 if an invalid url provided

--- tests/presentation/controllers/addFeedController.spec.ts
+++ tests/presentation/controllers/addFeedController.spec.ts
@@ -1,13 +1,30 @@
+import { UrlValidator } from '@/presentation/protocols/urlValidator'
 import { AddFeedController } from '@/presentation/controllers/feed/addFeedController'
 import { MissingParamError } from '@/presentation/errors/missingParamError'
+import { InvalidParamError } from '@/presentation/errors/invalidParamsErros'
 
-const makeSut = (): AddFeedController => {
-  return new AddFeedController()
+interface SutTypes {
+  sut: AddFeedController
+  urlValidatorStub: UrlValidator
+}
+const makeSut = (): SutTypes => {
+  class UrlValidatorStub implements UrlValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const urlValidatorStub = new UrlValidatorStub()
+  const sut = new AddFeedController(urlValidatorStub)
+  return {
+    sut,
+    urlValidatorStub
+  }
 }
 
 describe('AddFeedController', () => {
   test('Should return 400 if no url is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         description: 'anything',
@@ -20,7 +37,7 @@ describe('AddFeedController', () => {
   })
 
   test('Should return 400 if no description is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         url: 'anything',
@@ -30,5 +47,20 @@ describe('AddFeedController', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('description'))
+  })
+
+  test('Should return 400 if an invalid url is provided', () => {
+    const { sut, urlValidatorStub } = makeSut()
+    jest.spyOn(urlValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        url: 'anything',
+        location: 'anything',
+        description: 'anything'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('url'))
   })
 })
