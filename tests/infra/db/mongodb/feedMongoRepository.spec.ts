2f2e78c4fa6acf18afb84efc45ee87a5dfa30312
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-30 19:02:48 -0300

    test: ensure AddFeedMongoRepository add a new feed on add success

---
+++ tests/infra/db/mongodb/feedMongoRepository.spec.ts
@@ -0,0 +1,35 @@
+import { FeedMongoRepository } from '@/infra/db/mongodb/feedMongoRepository'
+import { MongoHelper } from '@/infra/db/mongodb/mongoHelper'
+import { Collection } from 'mongodb'
+
+let feedCollection: Collection
+
+const makeSut = (): FeedMongoRepository => {
+  return new FeedMongoRepository()
+}
+
+describe('Survey Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    feedCollection = await MongoHelper.getCollection('feed')
+    await feedCollection.deleteMany({})
+  })
+
+  test('Should add a feed on success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      url: 'valid_url',
+      description: 'valid_description',
+      location: 'valid_location'
+    })
+    const feed = await feedCollection.findOne({ url: 'valid_url' })
+    expect(feed).toBeTruthy()
+  })
+})
