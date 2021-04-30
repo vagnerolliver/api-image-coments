7f7a1c437d5d8a1155858d092a977a53b3d1c1a0
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-30 15:36:44 -0300

    test: ensure AddFeedController return badRequest error if validation fails

--- tests/presentation/controllers/addFeedController.spec.ts
+++ tests/presentation/controllers/addFeedController.spec.ts
@@ -1,5 +1,6 @@
 import { AddFeedController } from '@/presentation/controllers/feed/addFeedController'
 import { Validation, HttpRequest } from '@/presentation/protocols'
+import { badRequest } from '@/presentation/helpers/httpHelper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -40,5 +41,12 @@ describe('AddFeed Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
