a42220f0649eec90183e77fc920b657293d68116
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-30 15:36:29 -0300

    feat: ensure AddFeedController return badRequest error if validation fails

--- src/presentation/controllers/feed/addFeedController.ts
+++ src/presentation/controllers/feed/addFeedController.ts
@@ -1,9 +1,13 @@
+import { badRequest } from '@/presentation/helpers/httpHelper'
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/protocols'
 export class AddFeedController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return await new Promise(resolve => resolve(null))
   }
 }
