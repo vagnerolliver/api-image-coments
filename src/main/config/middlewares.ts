719473663913c3ab7d08b4ea9d390e127e0fbdee
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-30 19:57:49 -0300

    refactor: move all middlewares imports to a single file

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,5 @@
 import { Express } from 'express'
-import { bodyParser } from '@/main/middlewares/bodyParser'
-import { cors } from '@/main/middlewares/cors'
+import { bodyParser, cors } from '@/main/middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
