ce4b5a2a4c958f04ff48afc7c89c9f0462acdf2d
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-05-01 08:40:32 -0300

    feat: add AddFeed composition

--- src/main/routes/feedRoutes.ts
+++ src/main/routes/feedRoutes.ts
@@ -1,7 +1,7 @@
+import { adaptRoute } from '@/main/adapaters/expressRouteAdapater'
 import { Router } from 'express'
+import { makeAddFeedController } from '@/main/factories/controllers/addFeed/addFeedControllerFactory'
 
 export default (router: Router): void => {
-  router.post('/feed', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  router.post('/feed', adaptRoute(makeAddFeedController()))
 }
--- tests/main/routes/feedRoutes.test.ts
+++ tests/main/routes/feedRoutes.test.ts
@@ -1,15 +1,34 @@
 import request from 'supertest'
 import app from '@/main/config/app'
+import { MongoHelper } from '@/infra/db/mongodb/mongoHelper'
+import { Collection } from 'mongodb'
+
+let feedCollection: Collection
 
 describe('Feed Routes', () => {
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/feed')
-      .send({
-        url: 'valid_url',
-        description: 'valid_description',
-        location: 'valid_location'
-      })
-      .expect(200)
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    feedCollection = await MongoHelper.getCollection('surveys')
+    await feedCollection.deleteMany({})
+  })
+
+  describe('POST /feed', () => {
+    test('Should return 204 on add feed success', async () => {
+      await request(app)
+        .post('/api/feed')
+        .send({
+          url: 'valid_url',
+          description: 'valid_description',
+          location: 'valid_location'
+        })
+        .expect(204)
+    })
   })
 })
