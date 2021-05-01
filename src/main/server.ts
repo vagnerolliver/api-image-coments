b403b685821542d36fb1741a589fb332862a4a9e
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-05-01 05:25:20 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,14 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/mongoHelper'
 
-app.listen(5050, () => console.log('Server running at http://localhost:5050'))
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
+
+//   import app from './config/app'
+
+// app.listen(5050, () => console.log('Server running at http://localhost:5050'))
