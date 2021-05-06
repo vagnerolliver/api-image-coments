11eeb4f4b71d1f574f477ababe47accfc9dae9fa
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-06 19:54:27 -0300

    chore: add uri mongodb

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/backend-image-feed',
+  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/backend-image-feed',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://vagnerolliver:UVs59YPmBRzfcvr@cluster0.hsccl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   port: process.env.PORT || 5050
 }
