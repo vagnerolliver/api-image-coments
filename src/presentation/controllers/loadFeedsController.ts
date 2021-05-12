5c665872c3dd6c39dacef5b2350b9d6003eaf3f1
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-12 00:29:05 -0300

    feat: ensure LoadFeedsController return 204 if there is no content

--- src/presentation/controllers/loadFeedsController.ts
+++ src/presentation/controllers/loadFeedsController.ts
@@ -1,12 +1,12 @@
 import { LoadFeeds } from '@/domain/usecases/loadFeeds'
 import { Controller, HttpResponse } from '@/presentation/protocols'
-import { ok, serverError } from '@/presentation/helpers'
+import { ok, serverError, noContent } from '@/presentation/helpers'
 export class LoadFeedsController implements Controller {
   constructor (private readonly LoadFeeds: LoadFeeds) {}
   async handle (): Promise<HttpResponse> {
     try {
       const feeds = await this.LoadFeeds.load()
-      return ok(feeds)
+      return feeds.length ? ok(feeds) : noContent()
     } catch (error) {
       return serverError(error)
     }
