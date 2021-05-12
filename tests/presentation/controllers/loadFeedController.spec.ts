31c5094fa9f793f0db87bcdfddf4661fcb7a17e0
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-12 00:01:30 -0300

    test: ensure LoadFeedController return 200 on success

--- tests/presentation/controllers/loadFeedController.spec.ts
+++ tests/presentation/controllers/loadFeedController.spec.ts
@@ -1,32 +1,59 @@
 import { FeedModel } from '@/domain/models/feed'
-import { LoadFeed } from '@/domain/usecases/loadFeed'
+import { LoadFeeds } from '@/domain/usecases/loadFeeds'
 import { LoadFeedController } from '@/presentation/controllers/loadFeedController'
+import { ok } from '@/presentation/helpers'
 
-class LoadFeedSpy implements LoadFeed {
-  async load (): Promise<FeedModel> {
-    return null
+const makeFakeFeed = (): FeedModel[] => {
+  return [
+    {
+      id: 'idxx',
+      url: 'image1',
+      description: 'description1',
+      location: 'location1'
+    },
+    {
+      id: 'id2',
+      url: 'image2',
+      description: 'description2',
+      location: 'location2'
+    }
+  ]
+}
+
+const makeLoadFeed = (): LoadFeeds => {
+  class LoadFeedStub implements LoadFeeds {
+    async load (): Promise<FeedModel[]> {
+      return await new Promise(resolve => resolve(makeFakeFeed()))
+    }
   }
+  return new LoadFeedStub()
 }
 
 type SutTypes = {
   sut: LoadFeedController
-  loadFeedSpy: LoadFeed
+  loadFeedStub: LoadFeeds
 }
 
 const makeSut = (): SutTypes => {
-  const loadFeedSpy = new LoadFeedSpy()
-  const sut = new LoadFeedController(loadFeedSpy)
+  const loadFeedStub = makeLoadFeed()
+  const sut = new LoadFeedController(loadFeedStub)
   return {
     sut,
-    loadFeedSpy
+    loadFeedStub
   }
 }
 
 describe('LoadFeedController', () => {
-  test('Should calls LoadFeed usecase', async () => {
-    const { sut, loadFeedSpy } = makeSut()
-    const addSpy = jest.spyOn(loadFeedSpy, 'load')
+  test('Should calls LoadFeeds usecase', async () => {
+    const { sut, loadFeedStub } = makeSut()
+    const addSpy = jest.spyOn(loadFeedStub, 'load')
     await sut.handle()
     expect(addSpy).toHaveBeenCalled()
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(ok(makeFakeFeed()))
   })
 })
