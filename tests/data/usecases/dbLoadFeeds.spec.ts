5b984b588c3924eea9249b01285c1064e69aea18
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-12 22:42:23 -0300

    test: ensure DbLoadFeeds calls LoadFeedsRepository

---
+++ tests/data/usecases/dbLoadFeeds.spec.ts
@@ -0,0 +1,51 @@
+import { FeedModel } from '@/domain/models/feed'
+import { LoadFeedsRepository } from '@/data/protocols/db'
+import { DbLoadFeeds } from '@/data/usecases/dbLoadFeeds'
+
+const makeFakeFeeds = (): FeedModel[] => {
+  return [
+    {
+      id: 'idxx',
+      url: 'image1',
+      description: 'description1',
+      location: 'location1'
+    },
+    {
+      id: 'id2',
+      url: 'image2',
+      description: 'description2',
+      location: 'location2'
+    }
+  ]
+}
+type SutTypes = {
+  sut: DbLoadFeeds
+  loadSurveysRepositoryStub: LoadFeedsRepository
+}
+
+const makeLoadFeedRepository = (): LoadFeedsRepository => {
+  class LoadSurveysRepositoryStub implements LoadFeedsRepository {
+    async loadAll (): Promise<FeedModel[]> {
+      return await new Promise(resolve => resolve(makeFakeFeeds()))
+    }
+  }
+  return new LoadSurveysRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysRepositoryStub = makeLoadFeedRepository()
+  const sut = new DbLoadFeeds(loadSurveysRepositoryStub)
+  return {
+    sut,
+    loadSurveysRepositoryStub
+  }
+}
+
+describe('DbLoadFeeds', () => {
+  test('Should call LoadFeedsRepository', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
+    await sut.load()
+    expect(addSpy).toHaveBeenCalled()
+  })
+})
