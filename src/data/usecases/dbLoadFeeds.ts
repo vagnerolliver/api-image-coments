aa63b7ae060f21df8bab2c09ab90ee4575358cf7
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-12 22:49:55 -0300

    feat: ensure DbLoadFeeds returns a list of feeds on success

--- src/data/usecases/dbLoadFeeds.ts
+++ src/data/usecases/dbLoadFeeds.ts
@@ -1,11 +1,10 @@
 import { LoadFeeds } from '@/domain/usecases/loadFeeds'
 import { FeedModel } from '@/domain/models/feed'
 import { LoadFeedsRepository } from '../protocols/db'
-
 export class DbLoadFeeds implements LoadFeeds {
   constructor (private readonly loadFeedsRepository: LoadFeedsRepository) {}
   async load (): Promise<FeedModel[]> {
-    await this.loadFeedsRepository.loadAll()
-    return null
+    const feeds = await this.loadFeedsRepository.loadAll()
+    return feeds
   }
 }
