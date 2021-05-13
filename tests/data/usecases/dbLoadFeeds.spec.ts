e96879cb50cd7753aa8bc8167e14b5f2225421b4
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-12 22:55:34 -0300

    feat: ensure DbLoadFeeds returns throws if LoadFeedRepository throws

--- tests/data/usecases/dbLoadFeeds.spec.ts
+++ tests/data/usecases/dbLoadFeeds.spec.ts
@@ -1,6 +1,7 @@
 import { FeedModel } from '@/domain/models/feed'
 import { LoadFeedsRepository } from '@/data/protocols/db'
 import { DbLoadFeeds } from '@/data/usecases/dbLoadFeeds'
+import { ok } from '@/presentation/helpers'
 
 const makeFakeFeeds = (): FeedModel[] => {
   return [
@@ -47,5 +48,18 @@ describe('DbLoadFeeds', () => {
     const addSpy = jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
     await sut.load()
     expect(addSpy).toHaveBeenCalled()
+  })
+
+  test('Should return a list of Feeds on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.load()
+    expect(surveys).toEqual(makeFakeFeeds())
+  })
+
+  test('Should throw if LoadFeedsRepository throws', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
   })
 })
