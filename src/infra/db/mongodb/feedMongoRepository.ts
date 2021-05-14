ff14881f502203e7b4ad157ae63b5ab6762abcaf
vagnerolliver2 <vagner.olliver@gmail.com>
2021-05-14 20:15:27 -0300

    feat: ensure FeedMongoRepository load all feeds

--- src/infra/db/mongodb/feedMongoRepository.ts
+++ src/infra/db/mongodb/feedMongoRepository.ts
@@ -1,10 +1,17 @@
 import { MongoHelper } from '@/infra/db/mongodb/mongoHelper'
-import { AddFeedRepository } from '@/data/protocols/db/addFeedRepository'
-import { AddFeed } from '@/domain/usecases/addFeed'
+import { AddFeedRepository, LoadFeedsRepository } from '@/data/protocols/db'
+import { AddFeed } from '@/domain/usecases'
+import { FeedModel } from '@/domain/models/feed'
 
-export class FeedMongoRepository implements AddFeedRepository {
+export class FeedMongoRepository implements AddFeedRepository, LoadFeedsRepository {
   async add (feedData: AddFeed.Params): Promise<void> {
-    const feedCollection = await MongoHelper.getCollection('feed')
+    const feedCollection = await MongoHelper.getCollection('feeds')
     await feedCollection.insertOne(feedData)
+  }
+
+  async loadAll (): Promise<FeedModel[]> {
+    const feedCollection = await MongoHelper.getCollection('feeds')
+    const feeds: FeedModel[] = await feedCollection.find().toArray()
+    return feeds
   }
 }
