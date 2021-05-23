3991480c4e4a0c6d82a291b335e40cce7d48ec44
vagnerolliver <vagner.olliver@yahoo.com.br>
2021-05-22 23:11:34 -0300

    feat: ensure FeedMongoRepository find feed by id with success

--- src/infra/db/mongodb/feedMongoRepository.ts
+++ src/infra/db/mongodb/feedMongoRepository.ts
@@ -1,9 +1,9 @@
 import { MongoHelper } from '@/infra/db/mongodb'
-import { AddFeedRepository, LoadFeedsRepository } from '@/data/protocols/db'
+import { AddFeedRepository, LoadFeedsRepository, LoadFeedByIdRepository } from '@/data/protocols/db'
 import { AddFeed } from '@/domain/usecases'
 import { FeedModel } from '@/domain/models'
 
-export class FeedMongoRepository implements AddFeedRepository, LoadFeedsRepository {
+export class FeedMongoRepository implements AddFeedRepository, LoadFeedsRepository, LoadFeedByIdRepository {
   async add (feedData: AddFeed.Params): Promise<void> {
     const feedCollection = await MongoHelper.getCollection('feeds')
     await feedCollection.insertOne(feedData)
@@ -13,5 +13,11 @@ export class FeedMongoRepository implements AddFeedRepository, LoadFeedsReposito
     const feedCollection = await MongoHelper.getCollection('feeds')
     const feeds: FeedModel[] = await feedCollection.find().toArray()
     return feeds
+  }
+
+  async loadById (id: string): Promise<FeedModel> {
+    const feedCollection = await MongoHelper.getCollection('feeds')
+    const feed: FeedModel = await feedCollection.findOne({ _id: id })
+    return feed
   }
 }
