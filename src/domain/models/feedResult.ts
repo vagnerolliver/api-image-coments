8040a47f09572a3d44bb96a82f8837676bdd5792
vagnerolliver <vagner.olliver@yahoo.com.br>
2021-05-23 20:20:04 -0300

    chore: remove unncessary usecase

--- src/domain/models/feedResult.ts
+++
@@ -1,6 +0,0 @@
-export type FeedResultModel = {
-  id: string
-  url: string
-  description: string
-  location: string
-}
--- src/domain/usecases/saveFeedResult.ts
+++
@@ -1,7 +0,0 @@
-import { FeedResultModel } from '@/domain/models'
-
-export type SaveFeedResultModel = Omit<FeedResultModel, 'id'>
-
-export interface SaveFeedResult {
-  save: (data: SaveFeedResultModel) => Promise<FeedResultModel>
-}
