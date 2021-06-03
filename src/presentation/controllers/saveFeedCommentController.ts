3415955013dff0f463433a023ffcdd243e11e8dd
vagnerolliver <vagner.olliver@yahoo.com.br>
2021-06-03 11:54:32 -0300

    feat: ensure SaveFeedComment throws if LoadFeedById throws

--- src/presentation/controllers/saveFeedCommentController.ts
+++ src/presentation/controllers/saveFeedCommentController.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers'
+import { forbidden, serverError } from '@/presentation/helpers'
 import { LoadFeedById } from '@/domain/usecases'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
@@ -7,10 +7,14 @@ export class SaveFeedCommentController implements Controller {
   constructor (private readonly loadFeedById: LoadFeedById) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const feedComment = await this.loadFeedById.loadById(httpRequest.params.feedId)
-    if (!feedComment) {
-      return forbidden(new InvalidParamError('feedId'))
+    try {
+      const feedComment = await this.loadFeedById.loadById(httpRequest.params.feedId)
+      if (!feedComment) {
+        return forbidden(new InvalidParamError('feedId'))
+      }
+      return null
+    } catch {
+      return serverError(new Error())
     }
-    return null
   }
 }
